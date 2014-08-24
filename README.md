735ff415f5f3bdf6dd80d0c4347d06461410312a
ashwiniraj01 <ashu.sraj@gmail.com>
2014-08-23 23:24:00 -0400

    third commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Practical Machine Learning
+---------------------------
+
+Author : Ashwini raj
+Date   : 08/23/2014
+
+Instructions
+-------------
+
+1) Clone the code using 'git clone https://github.com/ashwiniraj01/PracticalMachineLearning.git YOURDIRECTORY'
+
+2) Load your RStudio
+
+3) Set your working directory to YOURDIRECTORY using setwd("YOURDIRECTORY")
+
+4) Run file "submitScript.R"
---
+++ submitScript.R
@@ -0,0 +1,25 @@
+# Load library to run file using knit
+library(knitr)
+
+# Run the file to generate md and html files
+knit('pmacl.Rmd', output = 'pmacl.md')
+knit('pmacl.Rmd', output = 'pmacl.md')
+
+# Run the function provided by coursera for submission
+
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+
+
+x <- test_data
+x <- x[train_subset[train_subset!='classe']]
+answers <- predict(model.rf, newdata=x)
+
+answers
+
+pml_write_files(answers)
